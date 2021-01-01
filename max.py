1ed25386611a19e721b54f0349f6f398399eb188
atharva-01 <atharvagado4902@gamil.com>
2021-01-01 11:06:19 +0530

    initial commit

--- max.py
+++ max.py
@@ -1,3 +1,7 @@
+'''
+    Program to find the maximam from the given list
+'''
+
 def get_max(numbers):
     max = num[0]
     for i in range(1,len(num)):
@@ -5,5 +9,13 @@ def get_max(numbers):
             max = num[i]
     return max
 
-num = [58 , 89 ,56 ,98]
-print(get_max(num))
+num =[]
+
+while True :
+    num1 =input('Enter the number:')
+    if num1 == ' ':
+        break
+    else:
+        num.append(int(num1))
+
+print('Maximnum from given list is:',get_max(num))
